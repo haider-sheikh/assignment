edd1b693f5ac37aac1dacd4b6fabfdbc6eff0feb
Haider Shahid <haider.shahid@7vals.com>
2020-08-21 12:28:02 +0500

    First Commit

---
+++ Event.rb
@@ -0,0 +1,9 @@
+class Event
+  attr_accessor :name, :desc
+  def initialize(name,desc)
+    @name ,@desc = name, desc
+  end
+  def show
+    print "#{@name}\t\t #{@desc}\n"
+  end
+end
---
+++ assig.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+require_relative 'Calender'
+
+c = Calendar.new
+c.display_portal
+
+# date 0 check year
+# resue type
+#only one rescue code
+
